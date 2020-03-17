f66c464ca2576a5e4141052d83411640fcf50e66
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-17 17:09:50 -0300

    use event and add log

--- src/App.js
+++ src/App.js
@@ -10,24 +10,42 @@ function App() {
     radius: '0px',
   })
 
+  function log() {
+    state.map(value => {
+      console.log(value)
+    })
+  }
+
   return (
     <div className="App">
       <div id="main-box"></div>
 
       <span class="costumize-area">
-        Style <input name="style" type="text" value={state.style}/>
+        Style <input name="style" type="text" value={state.style} onChange={(event)=>{
+          setState({ style: event.target.value })
+          log() // LOG
+        }}/>
       </span>
       <br/>
       <span class="costumize-area">
-        Color <input name="color" type="color" />
+        Color <input name="color" type="color" onChange={(event)=>{
+          setState({ color: event.target.value })
+          log() // LOG
+        }}/>
       </span>
       <br/>
       <span class="costumize-area">
-        Size <input name="size" type="number" />
+        Size <input name="size" type="number" onChange={(event)=>{
+          setState({ size: event.target.value })
+          log() // LOG
+        }}/>
       </span>
       <br/>
       <span class="costumize-area">
-        Radius <input name="radius" type="number" />
+        Radius <input name="radius" type="number" onChange={(event)=>{
+          setState({ radius: event.target.value })
+          log() // LOG
+        }}/>
       </span>
       <br/>
 
