c5d49d4d7b7a181be95588d5e3668af04fce4a35
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-17 17:13:37 -0300

    remove log

--- src/App.js
+++ src/App.js
@@ -10,12 +10,6 @@ function App() {
     radius: '0px',
   })
 
-  function log() {
-    state.map(value => {
-      console.log(value)
-    })
-  }
-
   return (
     <div className="App">
       <div id="main-box"></div>
@@ -23,28 +17,24 @@ function App() {
       <span className="costumize-area">
         Style <input name="style" type="text" value={state.style} onChange={(event)=>{
           setState({ style: event.target.value })
-          log() // LOG
         }}/>
       </span>
       <br/>
       <span className="costumize-area">
         Color <input name="color" type="color" onChange={(event)=>{
           setState({ color: event.target.value })
-          log() // LOG
         }}/>
       </span>
       <br/>
       <span className="costumize-area">
         Size <input name="size" type="number" onChange={(event)=>{
           setState({ size: event.target.value })
-          log() // LOG
         }}/>
       </span>
       <br/>
       <span className="costumize-area">
         Radius <input name="radius" type="number" onChange={(event)=>{
           setState({ radius: event.target.value })
-          log() // LOG
         }}/>
       </span>
       <br/>
