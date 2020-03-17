70c2dc75e2fe70ac5b55ec147819db4853dda28c
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-17 16:54:16 -0300

    add input area

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ function App() {
   return (
     <div className="App">
       <div id="main-box"></div>
+
+      <span class="costumize-area">
+        Style <input name="style" type="text"/>
+      </span>
+      <br/>
+      <span class="costumize-area">
+        Color <input name="color" type="color" />
+      </span>
+      <br/>
+      <span class="costumize-area">
+        Size <input name="size" type="number" />
+      </span>
+      <br/>
+      <span class="costumize-area">
+        Radius <input name="radius" type="number" />
+      </span>
+      <br/>
+
     </div>
   );
 }
