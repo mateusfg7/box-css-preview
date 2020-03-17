3cfff3c76cf59bd11f7d2fda1306358971bf8f0e
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-17 18:01:25 -0300

    update radius input

--- src/App.js
+++ src/App.js
@@ -53,9 +53,10 @@ function App() {
       </div>
       <br/>
       <div className="costumize-area">
-        Radius <input name="radius" type="range" value={state.radius} step="1" min="0" max="52" onChange={(event)=>{
+        Radius <input name="radius" type="range" value={state.radius} step="1" min="0" max="150" onChange={(event)=>{
           setState({ radius: event.target.value })
           updateBorderRadius(event.target.value)
+          console.log(event.target.value)
         }}/>
       </div>
       <br/>
