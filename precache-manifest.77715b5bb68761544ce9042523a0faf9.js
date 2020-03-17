c776e32151d75e143570029678d01e5102e57d8e
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-17 18:17:56 -0300

    Updates

---
+++ precache-manifest.77715b5bb68761544ce9042523a0faf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "149f8f1b5f4f579c5224c02e998bc885",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72aa19c4209f417a91bd",
+    "url": "/static/css/main.52fc97d4.chunk.css"
+  },
+  {
+    "revision": "da7ef7c4daedc78bd622",
+    "url": "/static/js/2.cff3e818.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.cff3e818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72aa19c4209f417a91bd",
+    "url": "/static/js/main.c64433b8.chunk.js"
+  },
+  {
+    "revision": "b53587bca171407b9faa",
+    "url": "/static/js/runtime-main.028f1740.js"
+  }
+]);
