0f75e7b1d2b247c1fd1e884b539ca85bb4eab56a
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-19 16:42:58 -0300

    change radius type: from 'px' to '%' in updateBorderRadius function

--- src/functions/updateBorderRadius.js
+++ src/functions/updateBorderRadius.js
@@ -1,14 +1,14 @@
 export default function updateBorderRadius(tag, value, side) {
     if (side === 'top') {
-        tag.style.borderTopLeftRadius = value + 'px'
+        tag.style.borderTopLeftRadius = value + '%'
     }
     if (side === 'right') {
-        tag.style.borderTopRightRadius = value + 'px'
+        tag.style.borderTopRightRadius = value + '%'
     }
     if (side === 'bottom') {
-        tag.style.borderBottomRightRadius = value + 'px'
+        tag.style.borderBottomRightRadius = value + '%'
     }
     if (side === 'left') {
-        tag.style.borderBottomLeftRadius = value + 'px'
+        tag.style.borderBottomLeftRadius = value + '%'
     }
 }
