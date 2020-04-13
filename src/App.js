beca2fe18b640be519ddc49ae949affa77ed4f08
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-13 14:04:07 -0300

    update style

--- src/App.js
+++ src/App.js
@@ -289,20 +289,22 @@ function App() {
       
       </div>
       
-      <h3>CSS</h3>
-      <textarea id="text-to-copy" type="text" value={`
+      <div className="css-area" >
+        <h3>CSS</h3>
+        <textarea id="text-to-copy" type="text" value={`
 border-style: ${stateTop.style} ${stateRight.style} ${stateButtom.style} ${stateLeft.style};
 border-color: ${stateTop.color} ${stateRight.color} ${stateButtom.color} ${stateLeft.color};
 border-width: ${stateTop.size} ${stateRight.size} ${stateButtom.size} ${stateLeft.size};
 border-radius: ${stateTop.radius}% ${stateRight.radius}% ${stateButtom.radius}% ${stateLeft.radius}%;
-      `}/>
-      <br/>
+        `}/>
+        <br/>
+      </div>
       <button onClick={() => {
         const areaToCopy = document.getElementById("text-to-copy")
         areaToCopy.select()
         document.execCommand('copy')
       }}>Copy css to clipboard</button>
-      
+
     </div>
   );
 }
