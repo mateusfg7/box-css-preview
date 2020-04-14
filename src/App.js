0cb3b8c63e26b21c45bbbac181e0a0f514084211
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-14 10:10:41 -0300

    extract RightCustumizeSection

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import BoxPreview from './components/BoxPreview'
 import TopCustumizeSection from './components/CustumizeSection/Top';
+import RightCustumizeSection from './components/CustumizeSection/Right';
 import CssGenerator from './components/CssGenerator'
 
 import updateBorderStyle from './functions/BorderUpdates/updateBorderStyle'
@@ -51,49 +52,12 @@ function App() {
           stateTop={stateTop}
           setStateTop={setStateTop}
         />
+        <RightCustumizeSection
+          stateRight={stateRight}
+          setStateRight={setStateRight}
+        />
 
-        <div className="custumize-section">
-          <p>Righ</p>
-          <div className="costumize-area">
-            Style <select onChange={
-                    (event) => {
-                      setStateRight({ ...stateRight, style: event.target.value })
-                      updateBorderStyle(document.getElementById('main-box'), event.target.value, 'right')
-                    }
-                  }>
-                    <option value="solid" selected>solid</option>
-                    <option value="dashed">dashed</option>
-                    <option value="double">double</option>
-                    <option value="dotted">dotted</option>
-                    <option value="groove">groove</option>
-                    <option value="ridge">ridge</option>
-                    <option value="inset">inset</option>
-                    <option value="outset">outset</option>
-                    <option value="hidden">hidden</option>
-                  </select>
-          </div>
-          <br/>
-          <div className="costumize-area">
-            Color <input name="color" type="color" onChange={(event)=>{
-              setStateRight({ ...stateRight, color: event.target.value })
-              updateBorderColor(document.getElementById('main-box'), event.target.value, 'right')
-            }}/>
-          </div>
-          <br/>
-          <div className="costumize-area">
-            Size <input name="size" type="number" onChange={(event)=>{
-              setStateRight({ ...stateRight, size: event.target.value + 'px' })
-              updateBorderSize(document.getElementById('main-box'), event.target.value, 'right')
-            }}/>
-          </div>
-          <br/>
-          <div className="costumize-area">
-            Radius <input name="radius" type="range" value={stateRight.radius} step="1" min="0" max="100" onChange={(event)=>{
-              setStateRight({ ...stateRight, radius: event.target.value })
-              updateBorderRadius(document.getElementById('main-box'), event.target.value, 'right')
-            }}/> <span>{stateRight.radius+'%'}</span>
-          </div>
-        </div>
+        
 
         <div className="custumize-section">
           <p>Bottom</p>
