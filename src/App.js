6f2d9341df0447166cedc3fca4dbcba8947eedc9
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-14 10:18:15 -0300

    extract BottomCustumizeSection

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import BoxPreview from './components/BoxPreview'
 import TopCustumizeSection from './components/CustumizeSection/Top';
 import RightCustumizeSection from './components/CustumizeSection/Right';
+import BottomCustumizeSection from './components/CustumizeSection/Bottom';
 import CssGenerator from './components/CssGenerator'
 
 import updateBorderStyle from './functions/BorderUpdates/updateBorderStyle'
@@ -28,7 +29,7 @@ function App() {
     size: '5px',
     radius: '0',
   })
-  const [stateButtom, setStateButtom] = useState({
+  const [stateBottom, setStateBottom] = useState({
     style: 'solid',
     color: '#000',
     size: '5px',
@@ -56,51 +57,10 @@ function App() {
           stateRight={stateRight}
           setStateRight={setStateRight}
         />
-
-        
-
-        <div className="custumize-section">
-          <p>Bottom</p>
-          <div className="costumize-area">
-            Style <select onChange={
-                    (event) => {
-                      setStateButtom({ ...stateButtom, style: event.target.value })
-                      updateBorderStyle(document.getElementById('main-box'), event.target.value, 'bottom')
-                    }
-                  }>
-                    <option value="solid" selected>solid</option>
-                    <option value="dashed">dashed</option>
-                    <option value="double">double</option>
-                    <option value="dotted">dotted</option>
-                    <option value="groove">groove</option>
-                    <option value="ridge">ridge</option>
-                    <option value="inset">inset</option>
-                    <option value="outset">outset</option>
-                    <option value="hidden">hidden</option>
-                  </select>
-          </div>
-          <br/>
-          <div className="costumize-area">
-            Color <input name="color" type="color" onChange={(event)=>{
-              setStateButtom({ ...stateButtom, color: event.target.value })
-              updateBorderColor(document.getElementById('main-box'), event.target.value, 'bottom')
-            }}/>
-          </div>
-          <br/>
-          <div className="costumize-area">
-            Size <input name="size" type="number" onChange={(event)=>{
-              setStateButtom({ ...stateButtom, size: event.target.value + 'px' })
-              updateBorderSize(document.getElementById('main-box'), event.target.value, 'bottom')
-            }}/>
-          </div>
-          <br/>
-          <div className="costumize-area">
-            Radius <input name="radius" type="range" value={stateButtom.radius} step="1" min="0" max="100" onChange={(event)=>{
-              setStateButtom({ ...stateButtom, radius: event.target.value })
-              updateBorderRadius(document.getElementById('main-box'), event.target.value, 'bottom')
-            }}/> <span>{stateButtom.radius+'%'}</span>
-          </div>
-        </div>
+        <BottomCustumizeSection
+          stateBottom={stateBottom}
+          setStateBottom={setStateBottom}
+        />
 
         <div className="custumize-section">
           <p>Left</p>
@@ -150,7 +110,7 @@ function App() {
       <CssGenerator
         stateTop={stateTop}
         stateRight={stateRight}
-        stateButtom={stateButtom}
+        stateButtom={stateBottom}
         stateLeft={stateLeft}
       />
 
