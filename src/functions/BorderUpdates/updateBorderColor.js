16b20c01ef5594fd4e6acbc1f9f22791e06962cc
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-21 11:19:28 -0300

    clean code

--- src/functions/BorderUpdates/updateBorderColor.js
+++ src/functions/BorderUpdates/updateBorderColor.js
@@ -1,9 +1,23 @@
-export default function updateBorderColor(tag, value, side) {
-    const methods = {
-        'top'() {tag.style.borderTopColor = value},
-        'right'(){tag.style.borderRightColor = value},
-        'bottom'(){tag.style.borderBottomColor = value},
-        'left'(){tag.style.borderLeftColor = value},
-    }
-    methods[side]()
+export default function updateBorderColor(
+  parameterTag,
+  parameterValue,
+  parameterSide
+) {
+  const tag = parameterTag;
+
+  const methods = {
+    top() {
+      tag.style.borderTopColor = parameterValue;
+    },
+    right() {
+      tag.style.borderRightColor = parameterValue;
+    },
+    bottom() {
+      tag.style.borderBottomColor = parameterValue;
+    },
+    left() {
+      tag.style.borderLeftColor = parameterValue;
+    },
+  };
+  methods[parameterSide]();
 }
