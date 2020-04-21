7e4625cc98dc675104cd042e43488f207b8f85d1
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-21 12:52:06 -0300

    clean code

--- src/components/CssGenerator/index.js
+++ src/components/CssGenerator/index.js
@@ -1,27 +1,36 @@
-import React from 'react'
+import React from "react";
 
-import './style.css'
+import "./style.css";
 
-export default function CssGenerator({ stateTop, stateRight, stateButtom, stateLeft }) {
-    
-    const value =`
+export default function CssGenerator({
+  stateTop,
+  stateRight,
+  stateButtom,
+  stateLeft,
+}) {
+  const value = `
 border-style: ${stateTop.style} ${stateRight.style} ${stateButtom.style} ${stateLeft.style};
 border-color: ${stateTop.color} ${stateRight.color} ${stateButtom.color} ${stateLeft.color};
 border-width: ${stateTop.size} ${stateRight.size} ${stateButtom.size} ${stateLeft.size};
 border-radius: ${stateTop.radius}% ${stateRight.radius}% ${stateButtom.radius}% ${stateLeft.radius}%;
-    `
-    return (
-        <div>
-            <div className="css-area">
-                <h3>CSS</h3>
-                <textarea id="text-to-copy" type="text" value={value}/>
-                <br/>
-            </div>
-            <button onClick={() => {
-                const areaToCopy = document.getElementById("text-to-copy")
-                areaToCopy.select()
-                document.execCommand('copy')
-            }}>Copy css to clipboard</button>
-        </div>
-    )
+    `;
+  return (
+    <div>
+      <div className="css-area">
+        <h3>CSS</h3>
+        <textarea id="text-to-copy" type="text" value={value} />
+        <br />
+      </div>
+      <button
+        onClick={() => {
+          const areaToCopy = document.getElementById("text-to-copy");
+          areaToCopy.select();
+          document.execCommand("copy");
+        }}
+        type="button"
+      >
+        Copy css to clipboard
+      </button>
+    </div>
+  );
 }
