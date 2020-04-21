cb569311334230cca91bee98f65ce09282cfe683
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-21 11:25:52 -0300

    clean code

--- src/functions/SizeUpdates/changeBoxSize.js
+++ src/functions/SizeUpdates/changeBoxSize.js
@@ -1,27 +1,27 @@
 export default function changeBoxSize(direction, boxSize, setBoxSize) {
-    const mainBox = document.getElementById("main-box")
-    
-    const updateSize = {
-      "height-plus"() {
-        const newHeight = boxSize.heigth + 1
-        mainBox.style.height = newHeight + 'px'
-        setBoxSize({ ...boxSize, heigth: newHeight })
-      },
-      "height-minus"() {
-        const newHeight = boxSize.heigth - 1
-        mainBox.style.height = newHeight + 'px'
-        setBoxSize({ ...boxSize, heigth: newHeight })
-      },
-      "width-plus"(){
-        const newWidth = boxSize.width + 1
-        mainBox.style.width = newWidth + 'px'
-        setBoxSize({ ...boxSize, width: newWidth })
-      },
-      "width-minus"(){
-        const newWidth = boxSize.width - 1
-        mainBox.style.width = newWidth + 'px'
-        setBoxSize({ ...boxSize, width: newWidth })
-      }
-    }
-    updateSize[direction]()
+  const mainBox = document.getElementById("main-box");
+
+  const updateSize = {
+    "height-plus": () => {
+      const newHeight = boxSize.heigth + 1;
+      mainBox.style.height = `${newHeight}px`;
+      setBoxSize({ ...boxSize, heigth: newHeight });
+    },
+    "height-minus": () => {
+      const newHeight = boxSize.heigth - 1;
+      mainBox.style.height = `${newHeight}px`;
+      setBoxSize({ ...boxSize, heigth: newHeight });
+    },
+    "width-plus": () => {
+      const newWidth = boxSize.width + 1;
+      mainBox.style.width = `${newWidth}px`;
+      setBoxSize({ ...boxSize, width: newWidth });
+    },
+    "width-minus": () => {
+      const newWidth = boxSize.width - 1;
+      mainBox.style.width = `${newWidth}px`;
+      setBoxSize({ ...boxSize, width: newWidth });
+    },
+  };
+  updateSize[direction]();
 }
