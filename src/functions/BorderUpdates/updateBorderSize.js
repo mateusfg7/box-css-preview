93d8996da46185d4c081523a0f970e1c58cc4412
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-21 11:22:26 -0300

    clean code

--- src/functions/BorderUpdates/updateBorderSize.js
+++ src/functions/BorderUpdates/updateBorderSize.js
@@ -1,9 +1,23 @@
-export default function updateBorderSize(tag, value, side) {
-    const methods = {
-        'top'() {tag.style.borderTopWidth = value + 'px'},
-        'right'(){tag.style.borderRightWidth = value + 'px'},
-        'bottom'(){tag.style.borderBottomWidth = value + 'px'},
-        'left'(){tag.style.borderLeftWidth = value + 'px'},
-    }
-    methods[side]()
+export default function updateBorderSize(
+  parameterTag,
+  parameterValue,
+  parameterSide
+) {
+  const tag = parameterTag;
+
+  const methods = {
+    top() {
+      tag.style.borderTopWidth = `${parameterValue}px`;
+    },
+    right() {
+      tag.style.borderRightWidth = `${parameterValue}px`;
+    },
+    bottom() {
+      tag.style.borderBottomWidth = `${parameterValue}px`;
+    },
+    left() {
+      tag.style.borderLeftWidth = `${parameterValue}px`;
+    },
+  };
+  methods[parameterSide]();
 }
